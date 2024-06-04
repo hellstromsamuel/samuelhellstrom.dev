190fa37866c3df7fabada21a19729e1f81eed44f
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2024-06-04 23:26:38 +0200

    background in layout div

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,9 @@
 <div class="flex min-h-screen flex-col">
 	<Header />
 
-	<main class="mx-auto flex w-full max-w-2xl flex-1 flex-col flex-col gap-8 px-4 py-8">
+	<main
+		class="mx-auto flex w-full max-w-2xl flex-1 flex-col flex-col gap-8 bg-background px-4 py-8"
+	>
 		<slot />
 	</main>
 </div>
