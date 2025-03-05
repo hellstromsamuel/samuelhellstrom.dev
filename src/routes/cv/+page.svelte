89f3773e59ab34892f3e3d8f65d183d192cbf0c2
Samuel Hellstrøm <samuel.hellstrom@knowit.no>
2025-03-05 07:52:45 +0100

    adjusted /cv site to show ProjectPreview

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts" type="module">
-	import ProjectCard from "$lib/components/projects/ProjectCard.svelte";
-	import NavigateBackLink from "$lib/components/ui/link/NavigateBackLink.svelte";
+	import ProjectPreview from "$lib/components/projects/ProjectPreview.svelte";
 	import { navRoutes } from "$lib/utils/navigation.js";
 
 	export let data;
@@ -9,16 +8,13 @@
 	let pageTitle = navRoutes.find((route) => route.href === "/cv")?.text;
 </script>
 
-<NavigateBackLink href="/" label={navRoutes.find((route) => route.href === "/")?.text} />
-<h1>{pageTitle}</h1>
+<div class="space-y-2">
+	<h1>{pageTitle}</h1>
+	<hr />
+</div>
 
-<div class="grid gap-8">
-	<div>
-		<p class="mb-2 text-sm font-bold">Prosjekter ({projects.length})</p>
-		<div class="flex flex-col gap-6">
-			{#each projects as project}
-				<ProjectCard {project} />
-			{/each}
-		</div>
-	</div>
+<div class="space-y-8">
+	{#each projects as project}
+		<ProjectPreview {project} />
+	{/each}
 </div>
