a960ecba885703f09be30473827b9caa6cf0a18f
Samuel Hellstrøm <samuel.hellstrom@knowit.no>
2025-03-05 07:51:54 +0100

    new component ProjectPreview (based on ProjectCard)

---
+++ src/lib/components/projects/ProjectPreview.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" type="module">
+	import type { Project } from "$lib/types/sanity/Project";
+	import ProjectKeywords from "./ProjectKeywords.svelte";
+
+	export let project: Project;
+</script>
+
+<div class="relative flex flex-col gap-6">
+	<div class="flex items-center gap-4 md:mr-12 md:gap-6">
+		<div>
+			<h2 class="text-base md:text-2xl">
+				{project.role}
+			</h2>
+			<p class="mb-2 text-sm md:text-base">
+				{#if project.company}
+					{`${project.company.name}: `}
+				{/if}
+				{project.name}
+			</p>
+
+			<ProjectKeywords {project} keywordClass="text-xs md:text-base" />
+		</div>
+	</div>
+</div>
