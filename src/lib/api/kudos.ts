7e68b2c82f56060e38df6bb75ce205836d8f3224
Samuel Hellstrøm <samuel.hellstrom@knowit.no>
2025-03-05 07:11:02 +0100

    deleted kudos.ts

--- src/lib/api/kudos.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from '$lib/supabaseClient';
-
-export async function getNumberOfKudosClicks() {
-	let { data } = await supabase.from('kudos_clicks').select('count');
-	if (!data) return null;
-	const numberOfKudosClicks = data[0].count;
-	return numberOfKudosClicks;
-}
