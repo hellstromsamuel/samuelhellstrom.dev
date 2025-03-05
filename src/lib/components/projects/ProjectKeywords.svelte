5068c2f8a243291f63b4b125cdbbfc236fe0b884
Samuel Hellstrøm <samuel.hellstrom@knowit.no>
2025-03-05 07:51:23 +0100

    removed keyword limit

--- src/lib/components/projects/ProjectKeywords.svelte
+++ src/lib/components/projects/ProjectKeywords.svelte
@@ -2,21 +2,12 @@
 	import { cn } from "$lib/utils/tailwind";
 
 	export let project;
-	export let limit = -1;
 	export let keywordClass = "";
-
-	$: keywordsToShow = project.keywords.slice(0, limit);
 </script>
 
 <div class="flex flex-wrap gap-2">
-	{#each keywordsToShow as keyword}
+	{#each project.keywords as keyword}
 		<span class={cn("rounded bg-gray-100 px-2 dark:bg-gray-800", keywordClass)}>{keyword.name}</span
 		>
 	{/each}
-
-	{#if limit !== -1 && project.keywords.length > limit}
-		<span class={cn("rounded bg-gray-100 px-2 dark:bg-gray-800", keywordClass)}
-			>+{project.keywords.length - limit}</span
-		>
-	{/if}
 </div>
